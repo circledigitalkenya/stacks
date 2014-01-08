1872f34c307041183e0dee3ab3fa5df8c2513774
Kamweti Muriuki <kamwetimuriuki@gmail.com>
2014-01-08 15:24:03 +0300

    this is ridiculous!

---
+++ www/js/notify.js
@@ -0,0 +1,14 @@
+
+
+// device APIs are available
+//
+// Wait for Cordova to load
+//
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// Cordova is ready
+//
+function onDeviceReady() {
+    console.log("RRRRRRRRRRRRRRRRRRRRRRRRR");
+    $('.main-window').css('background','yellow');
+}
