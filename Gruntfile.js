ee5042c1a19dabeb231d6f971f40abde1c20a68b
Kamweti Muriuki <kamwetimuriuki@gmail.com>
2014-01-14 18:27:02 +0300

    initial angular install

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,9 +4,9 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
         watch: {
           files: [
-            '<%= jshint.files %>'
+            'www/**'
           ],
-          tasks: ['jshint'{% if (min_concat) { %}, 'concat', 'min'{% } %}]
+          tasks: ['shell']
         },
         shell: {
           _options: {
@@ -14,20 +14,15 @@ module.exports = function(grunt) {
             stdout: true
           },
           debug_ios: {
-            command: 'cordova build ios'
+            command: 'cordova prepare ios'
           }
         },
-    })    
+    });   
 
     grunt.loadNpmTasks('grunt-shell');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
-    // Custom tasks
-    grunt.registerTask('debug','Create a debug build', function(platform) {
-    grunt.task.run('shell:debug_' + platform);
-    });
-
     // Default task
-    grunt.registerTask('default');
+    grunt.registerTask('default', ['watch']);
 
 };
--- www/js/app.js
+++ www/js/app.js
@@ -1,94 +1,45 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-      var db = window.sqlitePlugin.openDatabase("ladders", "1.0", "Demo", -1);
-
-      db.transaction(createtables,errorCB); // create tables
-
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-       console.log('variable' + thisis);
-    }
-};
-app.initialize();
-
-StatusBar.styleLightContent();
-
-function  clickScan() {
-    var scanner = cordova.require("com.phonegap.plugins.barcodescanner.barcodescanner");
-   scanner.scan(
-      function (result) {
-          alert("We got a barcode\n" +
-                "Result: " + result.text + "\n" +
-                "Format: " + result.format + "\n" +
-                "Cancelled: " + result.cancelled);
-      },
-      function (error) {
-          alert("Scanning failed: " + error);
-      }
-   );
-}
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'ladders' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'ladders.services' is found in services.js
+// 'ladders.controllers' is found in controllers.js
+angular.module('ladders', ['ui.router','ladders.services', 'ladders.controllers'])
 
 
-function createtables(tx){
-  // @todo: remove this line in production
-  tx.executeSql('DROP TABLE IF EXISTS books'); // only for debugging purposes, setup a fresh table on each app launch
-  
-  tx.executeSql(
-    'CREATE TABLE IF NOT EXISTS books'+
-    '('+
-      'id INTEGER PRIMARY KEY AUTOINCREMENT,'+
-      'isbn TEXT,'+
-      'title TEXT,'+
-      'description TEXT,'+
-      'image_path TEXT,'+
-      'author TEXT,'+
-      'pubdata TEXT'+ //pubdata will be a json string of publication data
-    ')'
-  );
-}
+.config(function($stateProvider, $urlRouterProvider) {
 
-// tansaction error callback
-function errorCB(){
-    console.log('error processing SQL: ' +err);
-}
+  // Use AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
 
-// Transaction success callback
-function successCB() {
-    console.log("success!");
-}
+    .state('home', {
+      url: "/",
+      templateUrl: "templates/home.html"
+    })
+    .state('book', {
+      url: "/book",
+      templateUrl: "templates/book.html"
+    })
+      .state('book.add', {
+        url: "/book/add",
+        templateUrl: "templates/book.add.html"
+      })
+      .state('book.scan', {
+        url: "/book/scan",
+        templateUrl: "templates/book.scan.html"
+      })
+      .state('book.search', {
+        url: "/book/search",
+        templateUrl: "templates/book.search.html"
+      });
 
+
+
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/home');
+
+});
 
