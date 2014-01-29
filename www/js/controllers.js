c8ca53beb43f67726df37555df86032502cb53e8
Kamweti Muriuki <kamwetimuriuki@gmail.com>
2014-01-29 18:45:42 +0300

    added scan and search localy and from amazon

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,7 +9,7 @@ angular.module('ladders.controllers', [])
       .then(
           function success(response, status,headers, config){
             if( response.data.status == 'success' && response.data.result.length > 0 ) {
-              BookService.setResults(response.data.result); //cachec the results
+              BookService.setResults(response.data.result); //cache the results
               $location.path('/search/results');
             } else {
               $location.path('/search/noresults');  // no results page
@@ -19,7 +19,6 @@ angular.module('ladders.controllers', [])
             //notify alert, could not connect to remote server
           }
       );
-
 
     }
 
@@ -40,52 +39,102 @@ angular.module('ladders.controllers', [])
       })
     }
 
-    $scope.scanBook = function(){
+    /**
+     * use the barcode scanner plugin to retreive books isbn
+     *   
+     * @param  bool amazon_search defaults to true, 
+     *                            if this is false we search for the isbn in users library
+     */
+    $scope.scanBook = function(amazon_search){
+
+      amazon_search = amazon_search || true;
+
       cordova.plugins.barcodeScanner.scan(
         function (result) {
           if( result.cancelled === 0 && result.format === 'EAN_13') {
+            
 
-            // search the book from our library
-            $scope
-            .searchISBNLocally(result.text)
-            .then(function(book){
-              if( book ) {
-                // if its a single book, show the book view
-                $location.path('/book?isbn=book.isbn');
-              } else {
-                // go to the scan/noresults page
-                $location.path('/scan/noresults')
-              }
-            });
+            if( amazon_search ) {
+              // search the book from amazon
+              $scope
+                .search_ISBN_From_Amazon(result.text)
+                .then(function(response){
+                  if( response ) {
+                    $location
+                      .path('/book')
+                      .search({
+                        isbn : result.text
+                      });
+                  } else {
+                    $location.path('/scan/noresults')
+                  }
+                }); 
 
+            } else {
+              // search the book from users library
+              $scope
+                .search_ISBN_Locally(result.text)
+                .then(function(response){
+                  if( response ) {
+                    // if its a single book, show the book view
+                    $location
+                      .path('/book')
+                      .search({
+                        isbn : result.text
+                      });
+
+                  } else {
+                    // go to the scan/noresults page
+                    $location.path('/scan/noresults')
+                  }
+                }); 
+
+            }
           } else {
             $location.path('')
           }
         }, 
         function (error) {
-            console.log("Scanning failed: " + error);
+          console.log("Scanning failed: " + error);
         }
       );
     }
 
 
-    $scope.searchISBNLocally = function(isbn){
+    $scope.search_ISBN_Locally = function(isbn){
       return database
               .query('SELECT id,isbn FROM books where isbn = "'+isbn+'" LIMIT 0, 1')
               .then(function(data){
                 if( data.length ) {
-                  return data[0]; //first index is the result
+                  BookService.setResults(data); //cache the results in the service
+                  return true;
                 } else {
                   return false;
                 }
               });
     }
 
+    $scope.search_ISBN_From_Amazon = function(isbn){
+
+      return BookService
+              .search_amazon(isbn)
+              .then(
+                function success(response, status,headers, config){
+                  if( response.data.status == 'success' && response.data.result.length > 0 ) {
+                    BookService.setResults(response.data.result);
+                    return true
+                  } else {
+                    return false
+                  }
+                }
+              );
+
+    }
+
 
 })
 .controller('SearchResults', function($scope,$location, BookService ){
   $scope.books = BookService.books;
-
 })
 .controller('LibraryController', function($scope, $location, database){
 
@@ -129,13 +178,13 @@ angular.module('ladders.controllers', [])
     $scope.book.exists_in_library = false;
 
     database
-    .query('SELECT id,isbn FROM books where isbn = "'+$scope.book.isbn+'" LIMIT 0, 1')
-    .then(function(data){
-      if( data.length ) {
-        $scope.book.id = data[0].id;
-        $scope.book.exists_in_library = true;
-      }
-    })
+      .query('SELECT id,isbn FROM books where isbn = "'+$scope.book.isbn+'" LIMIT 0, 1')
+      .then(function(data){
+        if( data.length ) {
+          $scope.book.id = data[0].id;
+          $scope.book.exists_in_library = true;
+        }
+      })
   }
 
   $scope.addToLibrary = function(){
