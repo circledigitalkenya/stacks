d5b75ef04f846e94f3f72f1f8543a6c4c4a0352d
Kamweti Muriuki <kamwetimuriuki@gmail.com>
2014-02-10 11:07:18 +0300

    doing away with the network information plugin for now

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ cordova plugin add https://github.com/wildabeast/BarcodeScanner
 cordova plugin add https://github.com/lite4cordova/Cordova-SQLitePlugin
 cordova plugin add org.apache.cordova.statusbar
 cordova plugin add org.apache.cordova.dialogs
-cordova plugin add org.apache.cordova.network-information
+#cordova plugin add org.apache.cordova.network-information # the newtwork info plugin comes with a whole set of issues when previewing the app on a browser http://stackoverflow.com/questions/20321532/phone-gap-device-ready-not-fired-after-5-seconds
 cordova plugin add org.apache.cordova.console
 cordova plugin add org.apache.cordova.contacts
